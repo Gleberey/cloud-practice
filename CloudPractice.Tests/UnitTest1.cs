41136af7fb024da7058e746cc86917cda86ce1ab
Hleb Hramiaka <hleb.hramiaka@stud.esdc.lt>
2026-05-11 18:03:07 +0300

    Add Docker infrastructure and unit tests

---
+++ CloudPractice.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿namespace CloudPractice.Tests;
+
+public class ServiceLogicTests
+{
+    [Test]
+    public void ListingMessage_ShouldContainListingText()
+    {
+        var listingName = "Test apartment";
+        var message = $"New listing created: {listingName}";
+
+        Assert.That(message, Does.Contain("Test apartment"));
+        Assert.That(message, Does.Contain("New listing created"));
+    }
+
+    [Test]
+    public void DisputeMessage_ShouldContainDisputeStatus()
+    {
+        var status = "Open";
+        var message = $"Dispute status: {status}";
+
+        Assert.That(message, Does.Contain("Open"));
+        Assert.That(message, Does.Contain("Dispute status"));
+    }
+
+    [Test]
+    public void QueueName_ShouldNotBeEmpty()
+    {
+        var queueName = "hlebhramiaka";
+
+        Assert.That(queueName, Is.Not.Empty);
+    }
+}
